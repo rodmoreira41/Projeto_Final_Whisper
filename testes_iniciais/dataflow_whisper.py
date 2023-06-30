f6f78c65b1d3bdf54202b5ed4fd7d7bef24d7b24
Rodrigo <a039291@ismai.pt>
2023-06-30 20:45:21 +0100

    update exemplo

--- testes_iniciais/dataflow_whisper.py
+++ testes_iniciais/dataflow_whisper.py
@@ -12,20 +12,17 @@ model = whisper.load_model("base")
 
 video = input("Coloque aqui o caminho do vídeo que pretende transcrever e carregue no enter: ")
 transcription = model.transcribe(video)
-text = transcription["text"]
 #exemplos_videos/howAIwillchangetheworld.mp3
-#exemplos_videos\X2Download.app-Does 1_0 = Infinity_ #shorts(720p).mp4
+#exemplos_videos/X2Download.app-Does 1_0 = Infinity_ #shorts(720p).mp4
 
 mycursor = database.cursor()
-
 sql_insert = "INSERT INTO transcriptions (text) VALUES (%s)"
-mycursor.execute(sql_insert, (text,))  # o texto precisa de ser convertido para tuple
-
+mycursor.execute(sql_insert, (transcription["text"],))  # o texto precisa de ser convertido para tuple
 database.commit() 
+
+print("Sucesso!")
 
 #mycursor.execute("SELECT * FROM transcriptions")
 #result = mycursor.fetchall()
 #for row in result:
   #print(row)
-
-print("Sucesso!")
